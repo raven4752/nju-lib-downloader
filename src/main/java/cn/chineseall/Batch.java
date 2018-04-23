88fb487b1379683ce9a9fd1f4b4d89415a38d85c
padeoe <padeoe@gmail.com>
2018-04-23 10:09:46 +0800

    批量下载

--- src/main/java/cn/chineseall/Batch.java
+++ src/main/java/cn/chineseall/Batch.java
@@ -1,23 +1,26 @@
 package cn.chineseall;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 /**
  * Created by padeoe on 2017/4/14.
  */
 public class Batch {
-    public static void main(String[] args) {
-        String className="D9";
-        if(args!=null&&args.length>0){
-            className=args[0];
+    public static void main(String[] args) throws IOException {
+        String className = "D9";
+        if (args != null && args.length > 0) {
+            className = args[0];
         }
         try {
-            Stream<String> bookStream=new Class(className).getNewBooks().flatMap(books -> books.stream().map(Book::toString));
+            new Class(className).getNewBooks().flatMap(books -> books.stream()).forEach(book ->
+                    {
+                        Downloader bookDownloader = new Downloader(book.getId(), new cn.chineseall.CoreService("Maskeney", "147258"));
+                        bookDownloader.setThreadNumber(2);
+                        bookDownloader.downloadBook();
+                    }
+            );
             //bookStream.forEach(book-> System.out.println(book));
-            Files.write(Paths.get("C:\\Users\\padeo\\Desktop\\法律书籍.txt"), (Iterable<String>)bookStream::iterator);
+            // Files.write(Paths.get("C:\\Users\\padeoe\\Desktop\\法律书籍.txt"), (Iterable<String>)bookStream::iterator);
 /*            System.out.println(className+"分类共"+books.size()+"本书");
             String finalClassName = className;
             books.parallelStream().*//*filter(book -> book.getAuthor().indexOf("(美")!=-1||book.getAuthor().indexOf("[美")!=-1).*//*forEach(book -> {
